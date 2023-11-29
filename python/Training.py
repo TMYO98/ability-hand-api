79eef84cd655f034bd30e10190babe3d53043ffa
Carlos Tamayo <jc.tamayo@hotmail.com>
2023-11-29 12:56:27 -0800

    new data

--- python/Training.py
+++ python/Training.py
@@ -6,7 +6,7 @@ import numpy as np
 import os
 
 # Step 1: Data Preprocessing
-folder_path = 'D:/Me/Trabajo 2023/Psyonic/ability-hand-api/python/DataForTraining/'  # Replace with your folder path
+folder_path = 'C:/Users/jctam/OneDrive/Escritorio/PSYONIC/ability-hand-api/python/DataForTraining/'  # Replace with your folder path
 file_names = [file for file in os.listdir(folder_path) if file.endswith(".csv")]
 data = pd.concat([pd.read_csv(folder_path+file) for file in file_names])
 
@@ -17,7 +17,7 @@ data['Power (W)'] = data['Power (W)'].abs()
 # Select relevant features
 X = data[['Power (W)', 'temperature', 'Elapsed-Time']]
 y = data['Motor-Temperature']
-print(X)
+# print(X)
 
 # Step 4: Split the Data
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
@@ -36,8 +36,8 @@ print(f"Mean Squared Error: {mse}")
 print(f"Root Mean Squared Error: {rmse}")
 print(f"R-squared (R2) Score: {r2}")
 
-# Step 7: Predict Motor Temperature for new data
-new_data = pd.DataFrame({'Power (W)': [new_power], 'temperature': [new_temperature], 'Elapsed-Time': [new_elapsed_time]})
-predicted_motor_temperature = model.predict(new_data)
+# # Step 7: Predict Motor Temperature for new data
+# new_data = pd.DataFrame({'Power (W)': [new_power], 'temperature': [new_temperature], 'Elapsed-Time': [new_elapsed_time]})
+# predicted_motor_temperature = model.predict(new_data)
 
-print(f"Predicted Motor Temperature: {predicted_motor_temperature[0]}")
+# print(f"Predicted Motor Temperature: {predicted_motor_temperature[0]}")
